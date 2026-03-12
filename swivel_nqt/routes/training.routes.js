d8007eb3765b652a4e59d294914958488e96cf59
Demonhari <149347540+Demonhari@users.noreply.github.com>
2026-03-12 18:35:54 +0530

    Revert "Swivel nqt"

--- swivel_nqt/routes/training.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/training.controller");
-
-router.get("/videos", controller.getVideos);
-router.post("/upload", controller.uploadVideo);
-
-module.exports = router;
