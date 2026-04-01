0118c4083e6a16b233691fd81f1d9dc604dce687
DurgaMahith12 <durga.mahith@swivelit.in>
2026-04-01 10:48:48 +0530

    Added changes in swivel_technology folder

--- swivel_academy_backend/config/db.js
+++ swivel_academy_backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Mahith@123",
     database: "swivel_it"
 });
 
